74e7c2847acc23a9d585b0070acf14b577caf6b5
ivancalvo-dbxs <ivan.calvo@databricks.com>
2026-07-22 16:17:45 -0600

    Add blog post title theme component to control title font size

---
+++ docs/starter-journey/src/theme/BlogPostItem/Header/Title/index.tsx
@@ -0,0 +1,17 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
+import type {Props} from '@theme/BlogPostItem/Header/Title';
+
+import styles from './styles.module.css';
+
+export default function BlogPostItemHeaderTitle({className}: Props): ReactNode {
+  const {metadata, isBlogPostPage} = useBlogPost();
+  const {title} = metadata;
+  const TitleHeading = isBlogPostPage ? 'h1' : 'h2';
+  return (
+    <TitleHeading className={clsx(styles.title, className)}>
+      {title}
+    </TitleHeading>
+  );
+}
